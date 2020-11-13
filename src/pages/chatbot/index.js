fbb531d0b00c027d220e57f532e4ffded4cac8bd
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-11-13 02:35:13 -0300

    Created repo Chatbot, implemented state in page Chatbot, implemented api from repo chatbot in page Chatbot

--- src/pages/chatbot/index.js
+++ src/pages/chatbot/index.js
@@ -1,12 +1,49 @@
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-import './style.css'
+import { getChatbotAnswer } from '../../repo/chatbotRepo'
 
 import HouseImg from '../../assets/img/house.svg'
 import ZeImg from '../../assets/img/ze.jpg'
 import MicrophoneImg from '../../assets/img/microphone.svg'
+import ArrowImg from '../../assets/img/arrow.svg'
+
+import './style.css'
 
 function Chatbot() {
+    const [messages, setMessages] = useState([])
+    const [inputText, setInputText] = useState('')
+
+    useEffect(() => {
+        setTimeout(() => {
+            setMessages([
+                { sender: 'ze', text: 'Opa! Tudo bem?', time: '10:00' },
+                { sender: 'ze', text: 'Que que eu posso te ajudar?', time: '10:00' }
+            ])
+        }, 500)
+    }, [])
+
+    const handleInputOnChange = (e) => {
+        setInputText(e.target.value)
+    }
+
+    const handleSendText = async (e) => {
+        e.preventDefault()
+
+        try {
+            const answer = await getChatbotAnswer(inputText)
+            setMessages([
+                ...messages,
+                { sender: 'user', text: inputText, time: '10:00' },
+                { sender: 'ze', text: answer, time: '10:00' }
+            ])
+
+            setInputText('')
+        } catch (error) {
+            alert('Houve um problema falando com o Seu Zé \nTenta de novo')
+        }
+    }
+    
     return (
         <div className='pagina-ze'>
             <div className="topo">
@@ -20,31 +57,27 @@ function Chatbot() {
             </div>
 
             <div className="mensagens">
-                <div className="mensagem ze">
-                    <span className="texto">Texto</span>
-                    <span className="hora">10:00</span>
-                </div>
-                <div className="mensagem usuario">
-                    <span className="texto">Texto</span>
-                    <span className="hora">10:00</span>
-                </div>
-                <div className="mensagem usuario">
-                    <span className="texto">Texto</span>
-                    <span className="hora">10:00</span>
-                </div>
-                <div className="mensagem ze">
-                    <span className="texto">Texto</span>
-                    <span className="hora">10:00</span>
-                </div>
+                {messages.map((message, i) => (
+                    <div className={`mensagem ${message.sender === 'ze' ? 'ze' : 'usuario'}`} key={i}>
+                        <span className="texto">{message.text}</span>
+                        <span className="hora">{message.time}</span>
+                    </div>
+                ))}
             </div>
 
-            <div className="fundo">
-                <input type="text" />
+            <form className="fundo" onSubmit={handleSendText}>
+                <input
+                    type="text"
+                    value={inputText}
+                    onChange={handleInputOnChange}
+                    onSubmit={handleSendText}
+                />
 
-                <button>
-                    <img src={MicrophoneImg} alt='microfone' />
+                <button type='submit'>
+                    {/* <img src={MicrophoneImg} alt='microfone' /> */}
+                    <img className='seta' src={ArrowImg} alt='seta' />
                 </button>
-            </div>
+            </form>
         </div>
     )
 }
---
+++ src/repo/chatbotRepo.js
@@ -0,0 +1,19 @@
+const endpoint = 'https://pague-ze.herokuapp.com/chatbot'
+
+const getChatbotAnswer = async (message) => {
+    const res = await fetch(`${endpoint}/messages`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            message
+        })
+    })
+    const answer = await res.json()
+    return answer.message
+}
+
+export {
+    getChatbotAnswer
+}
