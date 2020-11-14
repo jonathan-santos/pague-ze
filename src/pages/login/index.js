6aba73a966654ba7bd3fc61da746ca80205f54af
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-11-13 21:28:33 -0300

    Stylized page Login, implemented state

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,10 +1,50 @@
+import { useState } from 'react'
+
 import './style.css'
 
 function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleFormSubmit = e => {
+        e.preventDefault()
+    }
+
     return (
-        <>
-            LOGIN
-        </>
+        <div className='pagina-login'>
+            <form onSubmit={handleFormSubmit}>
+                <h1 className='titulo'>Login</h1>
+
+                <div className="input">
+                    <label htmlFor="email">Email:</label>
+
+                    <input
+                        id='email'
+                        name='email'
+                        type="text"
+                        placeholder="seuemail@email.com.br"
+                        required
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </div>
+
+                <div className="input">
+                    <label htmlFor="password">Password:</label>
+
+                    <input
+                        id='password'
+                        name='password'
+                        type="password"
+                        required
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                </div>
+
+                <button type="submit">Fazer login</button>
+            </form>
+        </div>
     )
 }
 
