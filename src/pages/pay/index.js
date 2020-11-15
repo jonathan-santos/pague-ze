4c5ec0865c85896b40182bc786ec7f4a75e6c216
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-11-15 19:12:56 -0300

    Change contents of page Pay

--- src/pages/pay/index.js
+++ src/pages/pay/index.js
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import Header from '../../components/header'
@@ -7,31 +6,15 @@ import TalkToChatbot from '../../components/talkToChatbot'
 import './style.css'
 
 function Pay({ history }) {
-    const [contact, setContact] = useState('')
-
-    const handleFormSubmit = e => {
-        e.preventDefault()
-
-        console.log("SUBMIT")
-    }
-
     return (
         <div className='pagina-pagar'>
             <Header history={history} />
 
-            <form className="conteudo escolher-contato" onSubmit={handleFormSubmit}>
+            <div className="conteudo escolher-contato">
                 <p className='titulo'>Quem você quer pagar?</p>
 
-                <input
-                    type="text"
-                    value={contact}
-                    onChange={e => setContact(e.target.value)}
-                    placeholder='Pesquisar contato...'
-                    required
-                />
-
-                <button type='submit' to='/' className='botao'>Escolher contatos</button>
-            </form>
+                <Link to='/contatos' className='botao'>Escolher contato</Link>
+            </div>
 
             <div className="conteudo outras-opcoes">
                 <p className="titulo">Outras opções</p>
